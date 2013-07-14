578e05fb5cee2a657003408a7c6e69f0051d286b
Jeremy Ward <jeremy.ward@digital-ocd.com>
2013-07-14 16:25:41 -0700

    Added additional static content routes and actions.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Blog::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  # Static content
+  get 'about' => 'content#about'
+  get 'projects' => 'content#projects'
+  get 'resume' => 'content#resume'
+
   # You can have the root of your site routed with "root"
   root :to => "content#index"
 
