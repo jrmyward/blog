0854d3d06fd7ea2645136a48db6774329964f443
Jeremy Ward <jeremy.ward@digital-ocd.com>
2013-07-14 20:01:04 -0700

    Implemented Authorization logic.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  ROLES = %w[admin author member]
+
   validates_presence_of :first_name, :last_name
+  validates_inclusion_of :role, :in => ROLES,
+                          :message => "The role <strong>{{value}}</strong> is not valid.",
+                          :if => "role.present?"
+
+  def admin?
+    role === "admin"
+  end
 end
