418768631babce3da75462b506a3d06e359264c0
Jeremy Ward <jeremy.ward@digital-ocd.com>
2013-07-25 17:02:44 -0700

    Implementing missing authorization logic.

--- spec/models/permissions/guest_permission_spec.rb
+++ spec/models/permissions/guest_permission_spec.rb
@@ -4,9 +4,9 @@ describe Permissions::GuestPermission do
   subject { Permissions.permission_for(nil) }
 
   it "allows sessions" do
-    should authorize(:sessions, :new)
-    should authorize(:sessions, :create)
-    should authorize(:sessions, :destroy)
+    should authorize("devise/sessions", :new)
+    should authorize("devise/sessions", :create)
+    should authorize("devise/sessions", :destroy)
   end
 
   it "allows posts" do
