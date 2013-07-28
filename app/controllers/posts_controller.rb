dd6430bfbcb45d22167835818a14c22ae626f909
Jeremy Ward <jeremy.ward@digital-ocd.com>
2013-07-27 23:47:35 -0700

    Initial UI improvements for the Post scaffold.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,7 +49,7 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      @post = Post.friendly.find(params[:id])
     end
 
 end
