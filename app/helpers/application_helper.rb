209d9ae34a68b85c1587012bd27a4d0db2bc7cd8
Jeremy Ward <jeremy.ward@digital-ocd.com>
2013-07-28 21:20:39 -0700

    Blog post scaffold UI styling.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,26 @@
 module ApplicationHelper
 
+  # Based on https://gist.github.com/1205828, in turn based on https://gist.github.com/1182136
+  class BootstrapLinkRenderer < ::WillPaginate::ActionView::LinkRenderer
+    protected
+
+    def html_container(html)
+      tag :div, tag(:ul, html), container_attributes
+    end
+
+    def page_number(page)
+      tag :li, link(page, page, :rel => rel_value(page)), :class => ('active' if page == current_page)
+    end
+
+    def gap
+      tag :li, link(super, '#'), :class => 'disabled'
+    end
+
+    def previous_or_next_page(page, text, classname)
+      tag :li, link(text, page || '#'), :class => [classname[0..3], classname, ('disabled' unless page)].join(' ')
+    end
+  end
+
   def gravatar_url(user, size = 26)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "https://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=mm"
@@ -41,5 +62,9 @@ module ApplicationHelper
     content_tag(:li, :class => class_name) do
       link_to link_text, link_path
     end
+  end
+
+  def page_navigation_links(pages)
+     will_paginate(pages, :class => 'pagination', :inner_window => 2, :outer_window => 0, :renderer => BootstrapLinkRenderer, :previous_label => '&larr;'.html_safe, :next_label => '&rarr;'.html_safe)
   end
 end
