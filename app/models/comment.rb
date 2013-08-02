6646e322a8b0e41c571a2ce31fe7a23de4fb4b33
Jeremy Ward <jeremy.ward@digital-ocd.com>
2013-08-01 21:23:06 -0700

    Initial functionality and specs for comments model.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,27 @@
 class Comment < ActiveRecord::Base
+  belongs_to :commentable, polymorphic: true
+
+  scope :published, lambda { where(:approved => true) }
+  scope :unapproved, lambda { where(:approved => false) }
+
+  validates_presence_of :body, :email, :name
+  validates_format_of :email,
+    :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+    :message => "Invalid email format.",
+    :if => "email.present?"
+
+  before_create :check_for_spam
+
+  def request=(request)
+    self.user_ip    = request.remote_ip
+    self.user_agent = request.env['HTTP_USER_AGENT']
+    self.referrer   = request.env['HTTP_REFERER']
+  end
+
+  private
+
+  def check_for_spam
+    self.approved = !self.spam?
+    true
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,57 @@
 require 'spec_helper'
+require "ostruct"
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should respond_to(:body) }
+  it { should respond_to(:email) }
+  it { should respond_to(:site_url) }
+  it { should respond_to(:user_ip) }
+  it { should respond_to(:user_agent) }
+  it { should respond_to(:referrer) }
+  it { should respond_to(:approved) }
+
+  it "sets request based attributes" do
+    comment = FactoryGirl.build(:comment)
+    comment.request = OpenStruct.new(remote_ip: 'ip', env: { 'HTTP_USER_AGENT' => 'agent', 'HTTP_REFERER' => 'referrer' })
+    comment.user_ip.should eq('ip')
+    comment.user_agent.should eq('agent')
+    comment.referrer.should eq('referrer')
+  end
+
+  describe "Akismet" do
+    it "should identify spam" do
+      comment = FactoryGirl.build(:comment, name: "Porn Master", body: "Porn plus Viagra is awesome!")
+      comment.request = OpenStruct.new(remote_ip: '127.0.0.1', env: { 'HTTP_USER_AGENT' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:17.0) Gecko/20100101 Firefox/17.0', 'HTTP_REFERER' => 'https://myfitrx.dev/blog/articles/hello-world' })
+      comment.spam?.should be_true
+      comment.save
+      comment.approved.should be_false
+    end
+
+    it "should identify ham" do
+      comment = FactoryGirl.build(:comment)
+      comment.request = OpenStruct.new(remote_ip: '127.0.0.1', env: { 'HTTP_USER_AGENT' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:17.0) Gecko/20100101 Firefox/17.0', 'HTTP_REFERER' => 'https://myfitrx.dev/blog/articles/hello-world' })
+      comment.spam?.should be_false
+      comment.save
+      comment.approved.should be_true
+    end
+  end
+
+  describe "Validation" do |variable|
+    [:body, :email, :name].each do |attr|
+      it "#{attr} must have a value" do
+        comment = FactoryGirl.build(:comment, attr => nil)
+        comment.should_not be_valid
+        comment.errors[attr].should_not be_nil
+      end
+    end
+
+    it "requires a valid email format" do
+      bad_emails = %w[user@foo,com user_at_foo.org example.user@foo.]
+      bad_emails.each do |bad_email|
+        comment_with_invalid_email = FactoryGirl.build(:comment, email: bad_email)
+        comment_with_invalid_email.should_not be_valid
+        comment_with_invalid_email.errors[:email].should_not be_nil
+      end
+    end
+  end
 end
