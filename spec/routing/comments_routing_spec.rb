5a486282f4c898e52986f793c4d04490f8f3298e
Jeremy Ward <jeremy.ward@digital-ocd.com>
2013-08-27 22:05:13 -0700

    Updated routes.
    Removed un-needed resources from post_comments. Created resources for Admin::Comments

--- spec/routing/comments_routing_spec.rb
+++ spec/routing/comments_routing_spec.rb
@@ -3,32 +3,12 @@ require "spec_helper"
 describe CommentsController do
   describe "routing" do
 
-    it "routes to #index" do
-      get("/blog/posts/1/comments").should route_to("comments#index", post_id: "1")
-    end
-
     it "routes to #new" do
       get("/blog/posts/1/comments/new").should route_to("comments#new", post_id: "1")
     end
 
-    it "routes to #show" do
-      get("/blog/posts/1/comments/1").should route_to("comments#show", post_id: "1", id: "1")
-    end
-
-    it "routes to #edit" do
-      get("/blog/posts/1/comments/1/edit").should route_to("comments#edit", post_id: "1", id: "1")
-    end
-
     it "routes to #create" do
       post("/blog/posts/1/comments").should route_to("comments#create", post_id: "1")
-    end
-
-    it "routes to #update" do
-      put("/blog/posts/1/comments/1").should route_to("comments#update", post_id: "1", id: "1")
-    end
-
-    it "routes to #destroy" do
-      delete("/blog/posts/1/comments/1").should route_to("comments#destroy", post_id: "1", id: "1")
     end
 
   end
