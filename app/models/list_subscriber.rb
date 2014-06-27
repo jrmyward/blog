877b5b0c544b21d69ae470ed98de769976df2bc2
Jeremy Ward <jrmy.ward@gmail.com>
2014-06-26 18:41:37 -0700

    Initial Newsletter opt in implementation.

--- app/models/list_subscriber.rb
+++ app/models/list_subscriber.rb
@@ -3,8 +3,11 @@ class ListSubscriber < ActiveRecord::Base
   validates_presence_of :email, :first_name
   validates_uniqueness_of :email
 
+  def newsletter_service
+    @newsletter ||= NewsletterService.new
+  end
+
   def subscribe_to_list
-    newsletter = NewsletterService.new
-    newsletter.subscribe(self)
+    newsletter_service.subscribe(self)
   end
 end
